09e12acbd47822effa3bf6deaa14a726ded5963d
Prathamesh More <pprathameshmore@outlook.com>
2023-01-05 12:13:05 +0530

    Initial commit

---
+++ src/components/CountrySelect.vue
@@ -0,0 +1,29 @@
+<template>
+    <select @change="onCountryChange()" v-model="selected" class="form-select block w-full border p-3 rounded mt-10">
+        <option value="0">
+            Select country
+        </option>
+        <option v-for="country in countries" :value="country.ID">
+            {{ country.Country }}
+        </option>
+    </select>
+</template>
+
+<script>
+export default {
+    name : "CountrySelect",
+    props: ['countries'],
+    data() {
+        return {
+            selected: 0
+        }
+    },
+    methods: {
+        onCountryChange() {
+            const country = this.countries.find((item) => item.ID === this.selected)
+            console.log('Country', country)
+            this.$emit('get-country', country)
+        }
+    }
+}
+</script>
---
+++ src/components/DataBoxes.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="grid md:grid-cols-2 gap-4">
+    <!-- Box 1-->
+    <div class="shadow-md bg-blue-100 p-10 text-center rounded">
+        <h3 class="text-3xl text-blue-900 font-bold md-4">
+            Cases
+            <div class="text-2xl mb-4">
+                <span class="font-bold">New:</span>
+                {{ numberWithCommas(stats.NewConfirmed) }} 
+                <span class="font-bold">Total:</span>
+                {{ numberWithCommas(stats.TotalConfirmed) }}
+            </div>
+        </h3>
+    </div>
+     <!-- Box 2-->
+    <div class="shadow-md bg-blue-200 p-10 text-center rounded">
+        <h3 class="text-3xl text-blue-900 font-bold md-4">
+            Deaths
+            <div class="text-2xl mb-4">
+                <span class="font-bold">New:</span>
+                {{ numberWithCommas(stats.NewDeaths) }} 
+                <span class="font-bold">Total:</span>
+                {{ numberWithCommas(stats.TotalDeaths) }}
+            </div>
+        </h3>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "DataBoxes",
+  props: ["stats"],
+  methods: {
+    numberWithCommas(num) {
+        return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+  }
+};
+</script>
